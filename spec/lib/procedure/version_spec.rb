993c9b1512c90924cf08188fcfebb03a11b757d5
Paweł Dąbrowski <dziamber@gmail.com>
2021-09-21 19:48:15 +0200

    Fix failing version spec

--- spec/lib/procedure/version_spec.rb
+++ spec/lib/procedure/version_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.describe Procedure::Version do
   describe '.to_s' do
     it 'returns current gem version' do
-      expect(described_class.to_s).to eq('0.0.2')
+      expect(described_class.to_s).to eq('0.0.4')
     end
   end
 end
